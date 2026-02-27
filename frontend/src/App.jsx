050dca28849c5fa81cd92a831c770cc5a1012fdb
styloq99-arch <styloq99@gmail.com>
2026-02-27 14:35:29 +0530

    Add SignUp Barber page 01

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import SignIn from "./pages/SignIn.jsx";
+import SignUpBarber from "./pages/SignUpBarber.jsx";
 
 
 export default function App() {
@@ -12,6 +13,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/signin" element={<SignIn />} />
+
+
+          <Route path="/signup-barber" element={<SignUpBarber />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
