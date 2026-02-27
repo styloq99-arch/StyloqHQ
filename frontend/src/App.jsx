264b10128fb54c785071aae418e6342a111d923c
Ranuthi Dahamsa <ranuthidahamsa12@gmail.com>
2026-02-27 13:38:32 +0530

    Add landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import "./global.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 
 export default function App() {
   return (
     <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<Index />} />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
   );
