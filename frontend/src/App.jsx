bd7aa95e599b0642204ac948cf472f09e6f42553
styloq99-arch <styloq99@gmail.com>
2026-02-27 12:56:06 +0530

    Frontend File Structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./global.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+    </BrowserRouter>
+  );
+}
